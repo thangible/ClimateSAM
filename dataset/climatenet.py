c03295db02bb0e9c13da7f83f0529752ee1c079b
thangible <thangible@gmail.com>
2025-11-05 22:15:48 +0100

    aa

--- dataset/climatenet.py
+++ dataset/climatenet.py
@@ -97,7 +97,7 @@ class ClimateDataset(Dataset):
             mask_before_shape = mask.shape
             data_before_shape = sam_input.shape 
             transform_dict = self.transforms(sam_input, mask, cgnet_input)
-            sam_input, mask, cgnet_input = transform_dict['input'], transform_dict['mask'], transform_dict['cgnet_input']
+            sam_input, mask, cgnet_input = transform_dict['input'], transform_dict['mask'], transform_dict['extra']
             assert sam_input.shape == data_before_shape, f"Data shape changed after transforms: {sam_input.shape} vs {data_before_shape}"
             assert mask.shape == mask_before_shape, f"Mask shape changed after transforms: {mask.shape} vs {mask_before_shape}"
 
