46a49ed1636a27bee70ec7869a7789bd524e9e18
thangible <thangible@gmail.com>
2025-11-16 23:57:23 +0100

    a

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import time
 def main():
     os.system('python3 train_unet.py --config input_config --run_name train_cgnet ')
     time.sleep(30)
-    os.system('python3 test.py --config input_config --run_name test_cgnet --prompt_type point ')
+    os.system('python3 test.py --config input_config --run_name test_cgnet_point --prompt_type point --max_epoch_num 2')
     time.sleep(30)
-    os.system('python3 test.py --config input_config --run_name test_cgnet --prompt_type bbox --max_epoch_num 2')
+    os.system('python3 test.py --config input_config --run_name test_cgnet_bbox --prompt_type bbox --max_epoch_num 2')
     time.sleep(30)
-    os.system('python3 test.py --config input_config --run_name test_cgnet --prompt_type mask --max_epoch_num 2')
+    os.system('python3 test.py --config input_config --run_name test_cgnet_mask --prompt_type mask --max_epoch_num 2')
 
 
 if __name__ == "__main__":
