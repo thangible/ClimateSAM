bae3408713e0f5314ca9c12271acbe721fab5039
thangible <thangible@gmail.com>
2026-03-15 23:08:51 +0100

    aa

--- train_script/official/train_adaptation.py
+++ train_script/official/train_adaptation.py
@@ -389,8 +389,8 @@ def main_worker(worker_id, worker_args):
         train_dataset = torch.utils.data.Subset(train_dataset, indices)
         print(f"Hyperparameter mode enabled: using {hp_size}/{orig_train_len} training samples (~{hp_size / orig_train_len * 100:.2f}%).")
         
-        max_epoch_num = 10
-        worker_args.valid_per_epochs = 2
+        max_epoch_num = 50
+        worker_args.valid_per_epochs = 5
         print(f"Hyperparameter mode: Setting max_epoch_num to {max_epoch_num} and valid_per_epochs to {worker_args.valid_per_epochs}") 
 
         
