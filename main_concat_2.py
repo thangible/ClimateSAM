291acd8a5bddb9a1958b2b35ec26bd9682bf7541
thangible <thangible@gmail.com>
2026-03-26 12:56:39 +0100

    aa

--- main_concat_2.py
+++ main_concat_2.py
@@ -8,11 +8,10 @@ def main():
     # os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat --sam_type vit_b  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_b_128_mlp075")
 
     # time.sleep(10)
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat --sam_type vit_l  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_l_128_mlp075")
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat --sam_type vit_l  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_l_128_mlp075 --focal_weight 10")
+
     time.sleep(10)
-    os.system("python train_script/official/train_adaptation.py --config input_config_concat --sam_type vit_l  --image_encoder_mlp_ratio 0.75 --run_name tune_token_infused_vit_l_128_mlp075")
-    time.sleep(10)
-    
+    os.system("python train_script/official/train_adaptation.py --config input_config_concat --sam_type vit_l  --image_encoder_mlp_ratio 0.75 --run_name tune_token_infused_vit_l_128_mlp075 --focal_weight 10")
 
 if __name__ == "__main__":
     main()
