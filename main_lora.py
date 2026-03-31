9fe0d8dcfb7d465fad47b5563cb5c996ab7ce257
joh68892 <johannes.helmholz@iapt.fraunhofer.de>
2026-03-31 10:06:40 +0000

    lora

---
+++ main_lora.py
@@ -0,0 +1,16 @@
+import os
+import time
+import random
+
+def main():
+    # LORA DUAL
+    os.system("python train_script/official/train_lora_sam_dual.py --config lora --sam_type vit_b --lora_r 32 --run_name lora_dual_vit_b_r32")
+    time.sleep(10)
+    os.system("python train_script/official/train_lora_sam_dual.py --config lora --sam_type vit_b --lora_r 64 --run_name lora_dual_vit_b_r64")
+    time.sleep(10)
+    os.system("python train_script/official/train_lora_sam_dual.py --config lora --sam_type vit_l --lora_r 32 --run_name lora_dual_vit_l_r32")
+    time.sleep(10)
+    os.system("python train_script/official/train_lora_sam_dual.py --config lora --sam_type vit_l --lora_r 16 --run_name lora_dual_vit_l_r16")
+
+if __name__ == "__main__":
+    main()
