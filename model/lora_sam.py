c2b95a3564caf15475f7d5110cd015737365f4dd
thangible <thangible@gmail.com>
2026-03-31 16:54:13 +0200

    input adatper

--- model/lora_sam.py
+++ model/lora_sam.py
@@ -222,7 +222,7 @@ class LoRAClimateSAMVanilla(nn.Module):
         if phase == 1:
             # Unfreeze image encoder LoRA adapters (already injected in-place)
             for n, c in self.named_children():
-                if n in ['image_encoder', 'mask_decoder_tc', 'mask_decoder_ar', 'input_adapter']:
+                if n in ['image_encoder', 'mask_decoder_tc', 'mask_decoder_ar']:
                     c.train(mode=mode)
                 else:
                     c.eval()
@@ -236,11 +236,28 @@ class LoRAClimateSAMVanilla(nn.Module):
                                 p.requires_grad = True
             # also unfreeze input_adapter and mask decoders
             for n, c in self.named_children():
-                if n in ['input_adapter', 'mask_decoder_tc', 'mask_decoder_ar']:
+                if n in ['mask_decoder_tc', 'mask_decoder_ar']:
                     for p in c.parameters():
                         p.requires_grad = True
             if verbose:
-                print("Phase 1: training LoRA adapters + input_adapter + mask decoders")
+                print("Phase 1: training LoRA adapters + mask decoders")
+                
+        if phase == 2:
+            for n, c in self.named_children():
+                if n in ['image_encoder', 'mask_decoder_tc', 'mask_decoder_ar', 'input_adapter']:
+                    c.eval()
+                else:
+                    c.train(mode=mode)
+            # Only LoRA adapter params should require grad
+            for blk in self.image_encoder.blocks:
+                if hasattr(blk, 'attn') and hasattr(blk.attn, 'qkv'):
+                    qkv = blk.attn.qkv
+                    if isinstance(qkv, _LoRA_qkv):
+                        for p in qkv.parameters():
+                            if hasattr(p, 'is_lora') and p.is_lora:
+                                p.requires_grad = True
+            if verbose:
+                print("Phase 2: training LoRA adapters + mask decoders + input_adapter")
 
         elif phase == 3:
             if self.use_prompt_generator:
