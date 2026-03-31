4052aba79b3bd1e68ca589cb4ce3842a8f1dec24
Srusti Ujjini Math <srusti.ujjini.math@iapt.fraunhofer.de>
2026-03-31 10:36:50 +0000

    concat_2

--- main_concat_2.py
+++ main_concat_2.py
@@ -14,15 +14,13 @@ def main():
     # os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat --sam_type vit_l  --image_encoder_mlp_ratio 0.5 --run_name tune_token_concat_vit_l_mlp05")
     # time.sleep(10)
     # TEST ML RATIO CONCAT VIT B
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat_2 --sam_type vit_b  --image_encoder_mlp_ratio 0.5 --run_name tune_token_concat_vit_b_mlp05")
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 0.5 --run_name tune_token_concat_vit_b_mlp05")
     time.sleep(10)
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat_2 --sam_type vit_b  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_b_mlp075")
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_b_mlp075")
     time.sleep(10)
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_concat --sam_type vit_b  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_b_mlp1")
-    # TRAIN SINGLE TOKEN
-    # os.system("python train_script/official/train_sam_single.py --config input_config_concat_2 --sam_type vit_b  --image_encoder_mlp_ratio 0.5 --run_name tune_single_token_vit_b_mlp05")
-    # time.sleep(10)
-    # os.system("python train_script/official/train_sam_single.py --config input_config_concat_2 --sam_type vit_l  --image_encoder_mlp_ratio 0.5 --run_name tune_single_token_vit_l_mlp05")
-    # time.sleep(10)
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 1 --run_name tune_token_concat_vit_b_mlp1")
+    time.sleep(10)
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_l  --image_encoder_mlp_ratio 0.75 --run_name tune_token_concat_vit_l_mlp075")
+    
 if __name__ == "__main__":
     main()
