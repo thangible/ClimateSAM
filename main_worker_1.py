94ba4d2a5e9fc8b35bf84dcb0d46a28ff2e58a09
thangible <thangible@gmail.com>
2026-04-01 09:17:30 +0200

    Update batch configuration and increase sleep duration in training scripts

--- main_worker_1.py
+++ main_worker_1.py
@@ -22,31 +22,30 @@ def main():
     
     # INFUSED TEST MLP RATIO VIT B
     os.system("python train_script/official/train_adaptation.py --config input_config_official --sam_type vit_b --image_encoder_mlp_ratio 1 --lr 1e-3 --run_name infused_token_vit_b_mlp1")
-    time.sleep(10)
+    time.sleep(15)
     os.system("python train_script/official/train_adaptation.py --config input_config_official --sam_type vit_b --image_encoder_mlp_ratio 0.75 --lr 1e-3 --run_name infused_token_vit_b_mlp075")
-    time.sleep(10)
+    time.sleep(15)
     os.system("python train_script/official/train_adaptation.py --config input_config_official --sam_type vit_b --image_encoder_mlp_ratio 0.5 --lr 1e-3 --run_name infused_token_vit_b_mlp05")
-    time.sleep(10)
+    time.sleep(15)
     
     # LORA VIT B
     
-    # LORA SINGLE VIT B
-    os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_b --lora_r 32 --run_name lora_single_vit_b_r32")
-    time.sleep(10)
-    os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_b --lora_r 16 --run_name lora_single_vit_b_r16")
-    time.sleep(10)
+    # # LORA SINGLE VIT B
+    # os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_b --lora_r 32 --run_name lora_single_vit_b_r32")
+    # time.sleep(15)
+    # os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_b --lora_r 16 --run_name lora_single_vit_b_r16")
+    # time.sleep(15)
     
     # LORA DUAL VIT L
     os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_l --lora_r 32 --run_name lora_single_vit_l_r32")
-    time.sleep(10)
+    time.sleep(15)
     os.system("python train_script/official/train_lora_sam.py --config input_config_official --sam_type vit_l --lora_r 16 --run_name lora_single_vit_l_r16")
 
     # INFUSED TEST MLP RATIO VIT L
     os.system("python train_script/official/train_adaptation.py --config input_config_official --sam_type vit_l --image_encoder_mlp_ratio 0.5 --lr 1e-3 --run_name infused_token_vit_l_mlp05")
-    time.sleep(10)
+    time.sleep(15)
     os.system("python train_script/official/train_adaptation.py --config input_config_official --sam_type vit_l --image_encoder_mlp_ratio 0.75 --lr 1e-3 --run_name infused_token_vit_l_mlp075")
-    time.sleep(10)
-    
+    time.sleep(15)
 
 
 if __name__ == "__main__":
