f9b9bb6f1088c0c8240182ba940d68f755b2b17f
thangible <thangible@gmail.com>
2026-04-04 01:37:10 +0200

    fix: update prompt enlarge ratio and add smooth label option in training scripts

--- main_worker_2.py
+++ main_worker_2.py
@@ -16,20 +16,20 @@ def main():
     
     
     # TEST ML RATIO CONCAT VIT B
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 0.5 --run_name token_concat_vit_b_mlp05_CORRECTED")
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 0.5 --run_name token_concat_vit_b_mlp05_CORRECTED --smooth_label")
     time.sleep(15)
 
-    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 1 --run_name token_concat_vit_b_mlp1_CORRECTED")
+    os.system("python train_script/official/train_tune_token_concat.py --config input_config_official --sam_type vit_b  --image_encoder_mlp_ratio 1 --run_name token_concat_vit_b_mlp1_CORRECTED --smooth_label")
     time.sleep(15)
 
     
     # SINGLE SAM VIT B
-    os.system("python train_script/official/train_sam_single.py --config input_config_official --sam_type vit_b --image_encoder_mlp_ratio 0.5 --run_name single_sam_vit_b_CORRECTED")
+    os.system("python train_script/official/train_sam_single.py --config input_config_official --sam_type vit_b --image_encoder_mlp_ratio 0.5 --run_name single_sam_vit_b_CORRECTED --smooth_label")
 
     # LORA DUAL VIT B
-    os.system("python train_script/official/train_lora_sam_dual.py --config input_config_official --sam_type vit_b --lora_r 64 --run_name lora_dual_vit_b_r64_CORRECTED --gradient_accumulation_steps 16")
+    os.system("python train_script/official/train_lora_sam_dual.py --config input_config_official --sam_type vit_b --lora_r 64 --run_name lora_dual_vit_b_r64_CORRECTED --gradient_accumulation_steps 16 --smooth_label")
     time.sleep(15)
-    os.system("python train_script/official/train_lora_sam_dual.py --config input_config_official --sam_type vit_b --lora_r 32 --run_name lora_dual_vit_b_r32_CORRECTED --gradient_accumulation_steps 16")
+    os.system("python train_script/official/train_lora_sam_dual.py --config input_config_official --sam_type vit_b --lora_r 32 --run_name lora_dual_vit_b_r32_CORRECTED --gradient_accumulation_steps 16 --smooth_label")
 
     # # LORA DUAL VIT L
     # os.system("python train_script/official/train_lora_sam_dual.py --config input_config_official --sam_type vit_l --lora_r 32 --run_name lora_dual_vit_l_r32 --gradient_accumulation_steps 16")
