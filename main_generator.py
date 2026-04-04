1bc30e9018fa0134501a4c4fecebb685a2116c94
Srusti Ujjini Math <srusti.ujjini.math@iapt.fraunhofer.de>
2026-04-04 11:39:30 +0000

    chore: add commented example command for training generator

--- main_generator.py
+++ main_generator.py
@@ -2,6 +2,8 @@ import os
 import time
 import random
 
+# python train_script/official/train_generator.py --config input_config_generator_test --fuse_channels 128 --sam_type vit_b --run_name test --encoder_weights_name best_weights/infused_token_vitb_mlp1_best --image_encoder_mlp_ratio 1
+
 def main():
 
     os.system("python train_script/official/train_generator.py --config input_config_generator --fuse_channels 128 --sam_type vit_b --run_name generator_256_vit_b --encoder_weights_name infused_token_vitb_mlp1_best ")
